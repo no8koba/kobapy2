3670da4f695f9075bd13f390916105b864266154
no8koba <36755307+no8koba@users.noreply.github.com>
2019-06-26 14:20:24 +0900

    code change

--- index.php
+++ index.php
@@ -1,277 +1,35 @@
-<!DOCTYPE html>
-<html lang="ja">
-<head>
-<meta charset="UTF-8">
-<meta http-equiv="X-UA-Compatible" content="IE=edge">
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<title>生年月日から年齢計算</title>
-<meta name="description" content="生年月日から年齢計算WEBアプリの実験。" />
-<meta name="keywords" content="生年月日から年齢計算,javascript,php,jquery" />
-<link href="common/css/bootstrap.min.css" rel="stylesheet">
-<link href="common/css/bootstrap-theme.min.css" rel="stylesheet">
-<link href="common/css/common.css" rel="stylesheet">
-<!-- Le HTML5 shim, for IE6-8 support of HTML5 elements -->
-<!--[if lt IE 9]>
-<script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
-<![endif]-->
+$str = "オリジナル";
 
-</head>
-<body>
-<!-- Navbar
-		================================================== -->
-<nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
-	<div class="container-fluid">
-		<!-- Brand and toggle get grouped for better mobile display -->
-		<div class="navbar-header">
-			<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
-				<span class="sr-only">Toggle navigation</span>
-				<span class="icon-bar"></span>
-				<span class="icon-bar"></span>
-				<span class="icon-bar"></span>
-			</button>
-			<a class="navbar-brand" href="http://php-fan.org/">PHPサンプル実験室</a>
-		</div>
+//①文字コードを変換して保存してみよう！
 
-		<!-- Collect the nav links, forms, and other content for toggling -->
-		<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-			<ul class="nav navbar-nav">
-			<li class=""><a href="#" onClick="history.back(); return false;">戻る</a></li>
-				<li class="active"><a href="/sample_code_demo/birth.php">現在のページ</a></li>
-				<li><a href="http://php-fan.org/contact">お問い合わせ</a></li>
-				<li><a href="http://php-fan.org/php-sample">PHP-SAMPLE-PAGES</a></li>
-				 <li><a href="http://php-fan.org/download">ダウンロード</a></li>
-				<li><a href="http://php-fan.org/about">管理人について</a></li>
-			</ul>
-		</div><!-- /.navbar-collapse -->
-	</div><!-- /.container-fluid -->
-</nav>
-<div class="container" id="container">
-<header>
-<div class="page-header">
-<h1>生年月日から年齢計算</h1>
-<h2><small>西暦から年齢を計算させて出力させます。</small></h2>
-</div>
-</header>
-<ol class="breadcrumb">
-<li><a href="http://php-fan.org/">Home</a></li>
-<li class="active">生年月日から年齢計算</li>
-</ol>
-<section>
-<div class="row">
-<div class="col-sm-8">
-<h3></h3>
-<form id="form1" name="form1" method="post" action="" class="form-horizontal">
-<fieldset>
-<legend>生年月日から年齢を出します。</legend>
-<div class="form-group">
-<label class="col-sm-2 control-label" for="select01">誕生日</label>
-<div class="row">
-<div class="col-sm-2">
-<select name="year" id="year" class="form-control">
-<option value="1920">1920</option>
-<option value="1921">1921</option>
-<option value="1922">1922</option>
-<option value="1923">1923</option>
-<option value="1924">1924</option>
-<option value="1925">1925</option>
-<option value="1926">1926</option>
-<option value="1927">1927</option>
-<option value="1928">1928</option>
-<option value="1929">1929</option>
-<option value="1930">1930</option>
-<option value="1931">1931</option>
-<option value="1932">1932</option>
-<option value="1933">1933</option>
-<option value="1934">1934</option>
-<option value="1935">1935</option>
-<option value="1936">1936</option>
-<option value="1937">1937</option>
-<option value="1938">1938</option>
-<option value="1939">1939</option>
-<option value="1940">1940</option>
-<option value="1941">1941</option>
-<option value="1942">1942</option>
-<option value="1943">1943</option>
-<option value="1944">1944</option>
-<option value="1945">1945</option>
-<option value="1946">1946</option>
-<option value="1947">1947</option>
-<option value="1948">1948</option>
-<option value="1949">1949</option>
-<option value="1950">1950</option>
-<option value="1951">1951</option>
-<option value="1952">1952</option>
-<option value="1953">1953</option>
-<option value="1954">1954</option>
-<option value="1955">1955</option>
-<option value="1956">1956</option>
-<option value="1957">1957</option>
-<option value="1958">1958</option>
-<option value="1959">1959</option>
-<option value="1960">1960</option>
-<option value="1961">1961</option>
-<option value="1962">1962</option>
-<option value="1963">1963</option>
-<option value="1964">1964</option>
-<option value="1965">1965</option>
-<option value="1966">1966</option>
-<option value="1967">1967</option>
-<option value="1968">1968</option>
-<option value="1969">1969</option>
-<option value="1970">1970</option>
-<option value="1971">1971</option>
-<option value="1972">1972</option>
-<option value="1973">1973</option>
-<option value="1974">1974</option>
-<option value="1975">1975</option>
-<option value="1976">1976</option>
-<option value="1977">1977</option>
-<option value="1978">1978</option>
-<option value="1979">1979</option>
-<option value="1980">1980</option>
-<option value="1981">1981</option>
-<option value="1982">1982</option>
-<option value="1983">1983</option>
-<option value="1984">1984</option>
-<option value="1985">1985</option>
-<option value="1986">1986</option>
-<option value="1987">1987</option>
-<option value="1988">1988</option>
-<option value="1989">1989</option>
-<option value="1990">1990</option>
-<option value="1991">1991</option>
-<option value="1992">1992</option>
-<option value="1993">1993</option>
-<option value="1994">1994</option>
-<option value="1995">1995</option>
-<option value="1996">1996</option>
-<option value="1997">1997</option>
-<option value="1998">1998</option>
-<option value="1999">1999</option>
-<option value="2000">2000</option>
-<option value="2001">2001</option>
-<option value="2002">2002</option>
-<option value="2003">2003</option>
-<option value="2004">2004</option>
-<option value="2005">2005</option>
-<option value="2006">2006</option>
-<option value="2007">2007</option>
-<option value="2008">2008</option>
-<option value="2009">2009</option>
-<option value="2010">2010</option>
-<option value="2011">2011</option>
-<option value="2012">2012</option>
-<option value="2013">2013</option>
-<option value="2014">2014</option>
-<option value="2015">2015</option>
-<option value="2016">2016</option>
-<option value="2017">2017</option>
-<option value="2018">2018</option>
-<option value="2019">2019</option>
-</select><span class="help-block">年</span>
-</div>
-<div class="col-sm-2">
-<select name="month" id="month" class="form-control">
-<option value="1">1</option>
-<option value="2">2</option>
-<option value="3">3</option>
-<option value="4">4</option>
-<option value="5">5</option>
-<option value="6">6</option>
-<option value="7">7</option>
-<option value="8">8</option>
-<option value="9">9</option>
-<option value="10">10</option>
-<option value="11">11</option>
-<option value="12">12</option>
-</select><span class="help-block">月</span>
-</div>
-<div class="col-sm-2">
-<select name="day" id="day" class="form-control">
-<option value="1">1</option>
-<option value="2">2</option>
-<option value="3">3</option>
-<option value="4">4</option>
-<option value="5">5</option>
-<option value="6">6</option>
-<option value="7">7</option>
-<option value="8">8</option>
-<option value="9">9</option>
-<option value="10">10</option>
-<option value="11">11</option>
-<option value="12">12</option>
-<option value="13">13</option>
-<option value="14">14</option>
-<option value="15">15</option>
-<option value="16">16</option>
-<option value="17">17</option>
-<option value="18">18</option>
-<option value="19">19</option>
-<option value="20">20</option>
-<option value="21">21</option>
-<option value="22">22</option>
-<option value="23">23</option>
-<option value="24">24</option>
-<option value="25">25</option>
-<option value="26">26</option>
-<option value="27">27</option>
-<option value="28">28</option>
-<option value="29">29</option>
-<option value="30">30</option>
-<option value="31">31</option>
-</select><span class="help-block">日</span>
-</div></div></div>
-<div class="form-group">
-<div class="col-sm-offset-2 col-sm-10">
- <input type="submit" name="button" class="btn btn-primary" value="年齢を調べる" />
-</div>
-</div>
-</form>
-</div>
-<div class="col-sm-4">
-<p>スポンサーリンク</p>
-<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
-<!-- レスポンシブ -->
-<ins class="adsbygoogle"
-     style="display:block"
-     data-ad-client="ca-pub-0912205435588730"
-     data-ad-slot="8544961215"
-     data-ad-format="auto"></ins>
-<script>
-(adsbygoogle = window.adsbygoogle || []).push({});
-</script>
-</div></div>
-</section>
-<footer>
-<div class="cm">
-<p>スポンサーリンク</p>
-<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
-<ins class="adsbygoogle"
-     style="display:block"
-     data-ad-format="autorelaxed"
-     data-ad-client="ca-pub-0912205435588730"
-     data-ad-slot="3525756907"></ins>
-<script>
-     (adsbygoogle = window.adsbygoogle || []).push({});
-</script>
-</div>
-<div id="copy">Copyright(C) 2011-2019 <a href="https://php-fan.org/">PHP-FAN.ORG</a> All Right Reserved. The MIT License (MIT)</div>
-</footer>
-</div><script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
-<script src="common/js/bootstrap.min.js"></script>
-<script type="text/javascript" src="common/js/prettify.js"></script>
+// 内部エンコーディング(UTF-8)からEUC-JPに変換します
+$euc =  mb_convert_encoding($str, "EUC-JP");
+// UTF-8からSJISに変換します
+$sjis =  mb_convert_encoding($str, "SJIS", "UTF-8");
+
+// 変換した文字列をファイルに保存するとEUC-JPで保存されています
+file_put_contents("euc.txt", $euc);
+// 変換した文字列をファイルに保存するとSHIFT-JISで保存されています
+file_put_contents("sjis.txt", $sjis);
 
 
-<script>
-  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+// ② 実行ファイルとは異なる文字コードの文字列を変換して表示してみよう！
 
-  ga('create', 'UA-77675624-14', 'auto');
-  ga('send', 'pageview');
+// 先程保存した異なる文字コードのファイルから文字列を取得します
+$euc = file_get_contents("euc.txt");
+$sjis = file_get_contents("sjis.txt");
 
-</script>
+// それぞれ正しい文字コードからUTF-8に変換すると「オリジナル」が表示されます
+echo '------------ 正しい変換 ------------ '.PHP_EOL;
+echo mb_convert_encoding($euc, "UTF-8", "EUC-JP").PHP_EOL;
+echo mb_convert_encoding($sjis, "UTF-8", "SJIS").PHP_EOL;
 
-</body>
-</html>
+// 元の文字コードとは別の文字コードを指定して変換すると文字化けが発生します
+echo '------------ 誤った変換 ------------ '.PHP_EOL;
+echo mb_convert_encoding($euc, "UTF-8", "SJIS").PHP_EOL;
+echo mb_convert_encoding($sjis, "UTF-8", "EUC-JP").PHP_EOL;
+
+// 第3引数にコンマ区切りや配列で複数の文字コードを指定することもでき、複数の中から該当する文字コードでエンコーディングされます
+echo '------------ 複数の文字コードを指定して変換 ------------ '.PHP_EOL;
+echo mb_convert_encoding($euc, "UTF-8", "UTF-8,EUC-JP").PHP_EOL;
+echo mb_convert_encoding($sjis, "UTF-8", ["JIS", "SJIS"]).PHP_EOL;
